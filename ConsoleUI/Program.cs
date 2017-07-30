b4db3bc304b3d10a7f550b37e1cbe512c2eb73fd
dolg6 <dolg6@DESKTOP-MEAMC1D>
2017-07-30 17:03:29 +0300

    1

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task1;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer timer = new Timer();
+
+            MobilePhone mobilePhone = new MobilePhone();
+            mobilePhone.Register(timer);
+
+            Pager pager = new Pager();
+            pager.Register(timer);
+
+            Console.WriteLine("Start");
+            timer.SimulateNewTimer( 4, "Hi");
+        }
+    }
+}
